e04f8f5d632d53e2c3f3b1c24d89f90cf1570143
Anmol-Deep-xx <workanmoldeepkaur1103@gmail.com>
2025-06-25 18:45:12 +0530

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -2,5 +2,5 @@
 
 version=development
 
-docker build -t anmoldeepkaur/busapp:${version} .
+DOCKER_BUILDKIT=1 docker build -t anmoldeepkaur/busapp:${version} .
 docker push anmoldeepkaur/busapp:${version}
